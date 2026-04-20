31b85fe0a2c8dbdbf5c9816742426b0c8ed4e0b6
Yogesh Ramsingh Thakur <yogesh_ramsinghthakur@epam.com>
2026-04-20 18:00:21 +0530

    chore: initialize backend CDK project

---
+++ test/shop-backend-product-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopBackendProductService from '../lib/shop-backend-product-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-backend-product-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopBackendProductService.ShopBackendProductServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
