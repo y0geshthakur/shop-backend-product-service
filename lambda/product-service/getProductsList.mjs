41ceed5a10f55cc5b6921f721bb0593fd56ac5d3
Yogesh Ramsingh Thakur <yogesh_ramsinghthakur@epam.com>
2026-04-21 13:39:22 +0530

    init: main branch with only README

--- lambda/product-service/getProductsList.mjs
+++
@@ -1,6 +0,0 @@
-import { products } from "./data/products.mjs";
-import { createResponse } from "./utils/response.mjs";
-
-export const handler = async () => {
-  return createResponse(200, products);
-};
